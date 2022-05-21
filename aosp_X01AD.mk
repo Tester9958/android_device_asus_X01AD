fa756cf21fe63c409810a5943d89a7496a2e2b9e
Tester9958 <103098781+Tester9958@users.noreply.github.com>
2022-05-21 12:28:23 +0530

    Update aosp_X01AD.mk
    Face unlock

--- aosp_X01AD.mk
+++ aosp_X01AD.mk
@@ -29,10 +29,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
 DEVICE_MAINTAINER := Nishant
 
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifiers.
 PRODUCT_NAME := aosp_X01AD
